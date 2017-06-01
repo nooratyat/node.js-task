e8288d9c19529725ed816d7fe0b4714799db805b
noor atyat <nooratyat92@gmail.com>
2017-06-01 16:56:50 +0300

    commit for security issue

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var AWS = require('aws-sdk');
  var s3 = require('s3');
-AWS.config.update({accessKeyId:'AKIAICM7D3TQOUAI6SLQ', secretAccessKey: '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'});
+//commit this for security issue  
+// AWS.config.update({accessKeyId:'AKIAICM7D3TQOUAI6SLQ', secretAccessKey: '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'});
 var s3Stream = require('./node_modules/s3-upload-stream/lib/s3-upload-stream.js')(new AWS.S3());
 
 var multiparty = require('connect-multiparty');
